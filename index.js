681fa75f795d4f2c37fd32897df775e52fd0c5bc
Sergio Heredia <sergioheredia1996@gmail.com>
2023-08-04 20:54:03 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+const totalBatteries = batteryBatches.reduce(function (initialCount, batteryNumber) {
+    return initialCount + batteryNumber; 
+} ,0)
 // Code your solution here
